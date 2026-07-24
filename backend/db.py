2de688756f34a3b4c2f04efe3b4e9c1298cc0564
jeffrey Zhu <jeffreyzhu11@gmail.com>
2026-07-23 22:46:23 -0400

    Added DB functions for deleting images as well as functions for svgs. Working on svg functions for generating gcode from SVGs. Need to edit the images DB table as well as create a table for svgs.

--- backend/db.py
+++ backend/db.py
@@ -1,79 +1,130 @@
 from backend.model import Image
 
-def insert(conn, name, timestamp, image_url):
-    print("insert", name, timestamp, image_url)
-
-    # open a cursor to perform database operations
-    cur = conn.cursor()
-
-    # define SQL query using placeholders
-    insert_query = """
-                   INSERT INTO images (name, timestamp, image_url)
-                   VALUES (%s, %s, %s); \
-                   """
-
-    # data to insert (as a tuple)
-    data_to_insert = (name, timestamp, image_url)
-
+def image_insert(conn, name, timestamp, image_path, gcode_path):
     try:
-        # execute the query
-        cur.execute(insert_query, data_to_insert)
+        with conn.cursor() as cursor:
 
-        # commit the transaction to save changes
-        conn.commit()
+            #define and execute the INSERT query
+            insert_query = "INSERT INTO images (name, timestamp, image_path, gcode_path) VALUES (%s, %s, %s, %s)"
+            data_to_insert = (name, timestamp, image_path, gcode_path)
+
+            cursor.execute(insert_query, data_to_insert)
+            conn.commit()
 
     except Exception as error:
-        print(f"Error: {error}")
-        conn.rollback()  # rollback transaction if it fails
+        conn.rollback()
         raise error
 
-    finally:
-        # close the communication with the database
-        cur.close()
 
-
-def selectAll(conn):
+def get_images(conn):
     try:
-        # create a cursor object
-        cursor = conn.cursor()
+        with conn.cursor() as cursor:
+            # define and execute the SELECT query
+            query = "SELECT * FROM images ORDER BY timestamp;"
+            cursor.execute(query)
 
-        # define and execute the SELECT query
-        query = "SELECT * FROM images ORDER BY timestamp;"
-        cursor.execute(query)
+            # fetch all rows from the table
+            rows = cursor.fetchall()
 
-        # fetch all rows from the table
-        rows = cursor.fetchall()
-
-        # print or process your list of data
-        return [Image(row[0], row[1], row[2], row[3]) for row in rows]
+            # return data
+            return [Image(row[0], row[1], row[2], row[3], row[4]) for row in rows]
 
     except Exception as error:
         raise error
 
-    finally:
-        if 'cursor' in locals():
-            cursor.close()
 
-
-def select(conn, id):
+def get_image(conn, id):
     try:
-        # create a cursor object
-        cursor = conn.cursor()
+        with conn.cursor() as cursor:
+            # define and execute the SELECT query
+            query = "SELECT * FROM images WHERE id = %s ORDER BY timestamp;"
+            cursor.execute(query, (id,))
 
-        # define and execute the SELECT query
-        query = "SELECT * FROM images WHERE id = %s ORDER BY timestamp;"
-        cursor.execute(query, (id,))
+            # fetch all rows from the table
+            rows = cursor.fetchall()
+            row = rows[0]
 
-        # fetch all rows from the table
-        rows = cursor.fetchall()
-        row = rows[0]
-
-        # print or process your list of data
-        return Image(row[0], row[1], row[2], row[3])
+            # print or process your list of data
+            return Image(row[0], row[1], row[2], row[3], row[4])
 
     except Exception as error:
         raise error
 
-    finally:
-        if 'cursor' in locals():
-            cursor.close()
+
+def delete_image(conn, id):
+    try:
+        with conn.cursor() as cursor:
+            #define and execute the DELETE query
+            delete_query = "DELETE FROM images WHERE id = %s"
+            cursor.execute(delete_query, (id,))
+
+            return cursor.rowcount
+
+    except Exception as error:
+        raise error
+
+def svg_insert(conn, name, timestamp, svg_url):
+    try:
+        with conn.cursor() as cursor:
+            #define and execute the INSERT query
+            instert_query = "INSERT INTO images (name, timestamp, image_url) VALUES (%s, %s, %s)"
+            data_to_insert = (name, timestamp, svg_url)
+
+            cursor.execute(instert_query, data_to_insert)
+            conn.commit()
+
+    except Exception as error:
+        conn.rollback()
+        raise error
+
+
+def get_svgs(conn):
+    try:
+        with conn.cursor() as cursor:
+            # define and execute the SELECT query
+            query = "SELECT * FROM images ORDER BY timestamp;"
+            cursor.execute(query)
+
+            # fetch all rows from the table
+            rows = cursor.fetchall()
+
+            # return data
+            return [Image(row[0], row[1], row[2], row[3]) for row in rows]
+
+    except Exception as error:
+        raise error
+
+
+def get_svg(conn, id):
+    try:
+        with conn.cursor() as cursor:
+            # define and execute the SELECT query
+            query = "SELECT * FROM images WHERE id = %s ORDER BY timestamp;"
+            cursor.execute(query, (id,))
+
+            # fetch all rows from the table
+            rows = cursor.fetchall()
+            row = rows[0]
+
+            # print or process your list of data
+            return Image(row[0], row[1], row[2], row[3])
+
+    except Exception as error:
+        raise error
+
+
+def delete_svg(conn, id):
+    try:
+        with conn.cursor() as cursor:
+            #define and execute the DELETE query
+            delete_query = "DELETE FROM svgs WHERE id = %s"
+            cursor.execute(delete_query, (id,))
+
+            return cursor.rowcount
+
+    except Exception as error:
+        raise error
+
+
+#Need to create a table for svgs
+#Need to extend colum for gcode for images
--- backend/main.py
+++ backend/main.py
@@ -8,8 +8,10 @@ import uvicorn
 from fastapi import FastAPI, HTTPException, File, UploadFile
 from starlette import status
 
-from backend.db import insert, selectAll, select
+from backend.db import image_insert, get_images, get_image
 from backend.model import Image
+from src.image_processing import generate_gcode_from_image
+from src.svg_processing import generate_gcode_from_svg
 
 app = FastAPI()
 
@@ -26,15 +28,24 @@ conn = psycopg2.connect(
 # upload image to database
 @app.post("/image/", status_code=201)
 async def create_image(file: Annotated[UploadFile, File()]):
+
+    # save the uploaded file to the "data/image_inputs" directory
     filename = file.filename
     file_path = Path(__file__).parent / "data" / "image_inputs" / filename
+
+    # gcode file name
+    gcode_filename = file_path.with_suffix(".gcode")
 
     with open(file_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
     try:
+        # convert image to gcode and get timestamp
+        generate_gcode_from_image(gcode_filename)
         current_time = datetime.now(timezone.utc)
-        insert(conn, filename, current_time, str(file_path))
+
+        # insert image into database
+        image_insert(conn, filename, current_time, str(file_path), gcode_filename)
 
     except Exception as e:
         raise HTTPException(
@@ -45,9 +56,9 @@ async def create_image(file: Annotated[UploadFile, File()]):
 
 # get all images in database
 @app.get("/image/", response_model=list[Image])
-def list_all():
+def list_images():
     try:
-        return selectAll(conn)
+        return get_images(conn)
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -57,9 +68,9 @@ def list_all():
 
 # get a specific image from database
 @app.get("/image/{id}", response_model=Image, status_code=200)
-def list(id: int):
+def list_image(id: int):
     try:
-        return select(conn, id)
+        return get_image(conn, id)
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -67,9 +78,55 @@ def list(id: int):
         )
 
 
+# delete a specific image from database
+@app.delete("/image/{id}", status_code=204)
+def delete_image(id: int):
+    try:
+        delete_image(conn, id)
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(e)
+        )
+
+
+# upload svg to database
+@app.post("/svg/", status_code=201)
+async def create_svg(file: Annotated[UploadFile, File()]):
+    # save the uploaded file to the "data/svg_inputs" directory
+    filename = file.filename
+    file_path = Path(__file__).parent / "data" / "svg_inputs" / filename
+
+    # gcode file name
+    gcode_filename = file_path.with_suffix(".gcode")
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    try:
+        # convert svg to gcode and get timestamp
+        generate_gcode_from_svg(gcode_filename)
+        current_time = datetime.now(timezone.utc)
+
+        # insert image into database
+        image_insert(conn, filename, current_time, str(file_path), gcode_filename)
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+
+# get all svgs in database
+
+# get a specific svg from database
+
 if __name__ == "__main__":
     # Specify the port keyword argument here
     uvicorn.run("main:app", host="0.0.0.0", port=5050, reload=True)
 
     # DB close
     conn.close()
+
+#Need another endpoint for starting plotting
+
